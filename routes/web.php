0b25a84cca00dc5f2ccdec2d5a313a6d8026e902
Aurelien Roux <aureroux13@gmail.com>
2026-03-03 15:16:38 +0100

    login route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\Login;
 use App\Http\Controllers\Auth\Logout;
 use App\Http\Controllers\Auth\Register;
 use App\Http\Controllers\ChirpController;
@@ -24,4 +25,12 @@ Route::post('/register', Register::class)
 
 // Logout
 Route::post('/logout', Logout::class)
-    ->middleware('auth');
+    ->middleware('auth')
+    ->name('logout');
+
+// Login
+Route::view('/login', 'auth.login')
+    ->middleware('guest')
+    ->name('login');
+Route::post('login', Login::class)
+    ->middleware('guest');
